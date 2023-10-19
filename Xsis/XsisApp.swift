753447317f201e98b98158c7629f6d9007fc9f54
Rahmat Susanto <rarito.jogja@gmail.com>
2023-10-19 11:21:14 +0700

    Initial Commit

---
+++ Xsis/XsisApp.swift
@@ -0,0 +1,17 @@
+//
+//  XsisApp.swift
+//  Xsis
+//
+//  Created by Rahmat Susanto on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct XsisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
